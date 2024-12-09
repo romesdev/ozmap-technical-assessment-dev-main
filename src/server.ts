acb6f11422f1de3e2be5ce179a29fb84d87ccb20
romesdev <fs.romes@gmail.com>
2024-12-09 17:18:18 -0300

    chore: add base config setup, add gitignore, update docker compose with mongodb image, remove unused files and update package scripts

--- src/server.ts
+++ src/server.ts
@@ -1,64 +1,13 @@
-import * as app from 'express';
-import { UserModel } from './models';
+import app from './app';
+import mongoose from 'mongoose';
 
-const server = app();
-const router = app.Router();
+const PORT = 3000;
+const MONGO_URI = 'mongodb://localhost:27017/ozmap';
 
-const STATUS = {
-  OK: 200,
-  CREATED: 201,
-  UPDATED: 201,
-  NOT_FOUND: 400,
-  BAD_REQUEST: 400,
-  INTERNAL_SERVER_ERROR: 500,
-  DEFAULT_ERROR: 418,
-};
-
-router.get('/user', async (req, res) => {
-  const { page, limit } = req.query;
-
-  const [users, total] = await Promise.all([
-    UserModel.find().lean(),
-    UserModel.count(),
-  ]);
-
-  return res.json({
-    rows: users,
-    page,
-    limit,
-    total,
-  });
-});
-
-router.get('/users/:id', async (req, res) => {
-  const { id } = req.params;
-
-  const user = await UserModel.findOne({ _id: id }).lean();
-
-  if (!user) {
-    res.status(STATUS.INTERNAL_SERVER_ERROR).json({ message: 'Region not found' });
-  }
-
-  return user;
-});
-
-router.put('/users/:id', async (req, res) => {
-  const { id } = req.params;
-  const { update } = req.body;
-
-  const user = await UserModel.findOne({ _id: id }).lean();
-
-  if (!user) {
-    res.status(STATUS.DEFAULT_ERROR).json({ message: 'Region not found' });
-  }
-
-  user.name = update.name;
-
-  await user.save();
-
-  return res.sendStatus(201);
-});
-
-server.use(router);
-
-export default server.listen(3003);
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log('Connecting to MongoDB');
+    app.listen(PORT, () => console.log(`Server is running in ${PORT}`));
+  })
+  .catch((err) => console.error('Error on connect with DB', err));
