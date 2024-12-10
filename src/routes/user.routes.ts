1f1577c66b3c8e1f2e44a776e00fd0a2ac13b390
romesdev <fs.romes@gmail.com>
2024-12-09 21:50:56 -0300

    refactor: change naming in validation files from dto to schema

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import { UserController } from '../controllers/user.controller';
 import { validateData } from '../middlewares/validationMiddleware';
-import { saveUserSchema } from '../dtos/user.dto';
+import { saveUserSchema } from '../schemas/user.schema';
 
 const router = Router();
 
